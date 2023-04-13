110657e28e05e7c5b9a60b320468f266b20c1245
Tomas <tomasbalbinder@gmail.com>
2023-04-13 22:52:29 +0200

    task2 part2

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
+#part 1
 
 # first - opponent  
 # A - rock
@@ -19,9 +20,6 @@
 # 0 if you lost
 # 3 if the round was tied
 # 6 if you won
-
-
-
 
 
 def loading_input():
@@ -73,5 +71,58 @@ for i in new_list:
     else:
         score = score + (i[1])
 
+
+print(score)
+
+#part 2
+# X you lose 
+# Y draw
+# Z you win
+
+'''
+A - 1 X - 1 = rock - Z - 3
+A - 1 Y - 2 = rock - X - 1 + 3
+A - 1 Z - 3 = rock - Y - 2  + 6 
+
+B - 2 X - 1 = paper - X - 1
+B - 2 Y - 2 = paper - Y - 2 + 3
+B - 2 Z - 3 = paper - Z - 3 + 6
+
+C - 3 X - 1 = sccisors Y - 2
+C - 3 Y - 2 = sccisors Z - 3 + 3
+C - 3 Z - 3 = sccisors X - 1 + 6
+'''
+score = 0        
+
+for i in new_list:
+
+    if i[0] == 1 and i[1] == 1:
+        score = score + (3)
+
+    elif i[0] == 1 and i[1] == 2:
+        score = score + (1 + 3)
+
+    elif i[0] == 1 and i[1] == 3:
+        score = score + (2 + 6)  
+
+
+    elif i[0] == 2 and i[1] == 1:
+        score = score + (1)
+
+    elif i[0] == 2 and i[1] == 2:
+        score = score + (2 + 3)
+
+    elif i[0] == 2 and i[1] == 3:
+        score = score + (3 + 6)
+
+
+    elif i[0] == 3 and i[1] == 1:
+        score = score + (2)
+
+    elif i[0] == 3 and i[1] == 2:
+        score = score + (3 + 3)
+
+    elif i[0] == 3 and i[1] == 3:
+        score = score + (1 + 6)
 
 print(score)
